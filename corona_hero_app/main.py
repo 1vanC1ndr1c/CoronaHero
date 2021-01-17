ad9ab597360f8cb10248eeece25163934846a8ee
Zvonimir Zovak <zzovak@avaya.com>
2021-01-17 17:00:43 +0100

    fix infinite jump

--- corona_hero_app/main.py
+++ corona_hero_app/main.py
@@ -1,8 +1,8 @@
 import sys
 
-from corona_hero_app.level_1 import level_1
-from corona_hero_app.level_2 import level_2
-from corona_hero_app.level_5 import level_5
+from corona_hero_app.levels.level_1 import level_1
+from corona_hero_app.levels.level_2 import level_2
+from corona_hero_app.levels.level_5 import level_5
 from corona_hero_app.levels.test_level import start_test_level
 
 def main():
